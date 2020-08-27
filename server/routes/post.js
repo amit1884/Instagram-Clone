aa52b6bd5340ac2fe25624d474ecf361b91dc553
Amit Raj <49913746+amit1884@users.noreply.github.com>
2020-08-27 10:08:23 +0530

    client side added

--- server/routes/post.js
+++ server/routes/post.js
@@ -7,16 +7,48 @@ router.post('/createpost',requireLogin,(req,res)=>{
 
     const {title,body}=req.body;
     if(!title||!body){
-        return res.status(422).json({message:"Please fill all the fields"})
+        return res.status(401).json({message:"Please fill all the fields"})
     }
-    console.log(req.user)
-    res.send("Ok")
-    // const post=new Post({
-    //     title,
-    //     body,
-    //     psotedBy
-    // })
+    // console.log(req.user)
+    // res.send("Ok")
+
+    req.user.password=undefined;
+    const post=new Post({
+        title,
+        body,
+        postedBy:req.user
+    })
+    post.save()
+    .then(result=>{
+        res.json({post:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 })
 
+router.get('/allpost',requireLogin,(req,res)=>{
+
+    Post.find()
+    .populate("postedBy","_id name")
+    .then(posts=>{
+         res.json({posts})   
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+router.get('/mypost',requireLogin,(req,res)=>{
+
+    Post.find({postedBy:req.user._id})
+    .populate("postedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 
 module.exports=router;
