146f25af827fe6ff71b24ebab4d953aae1dfc068
Amit Raj <49913746+amit1884@users.noreply.github.com>
2020-08-27 12:42:28 +0530

    basic routes added in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
-
+import {BrowserRouter,Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Screens/Home';
+import Login from './components/Screens/Login';
+import SignUp from './components/Screens/SignUp';
+import Profile from './components/Screens/Profile';
 function App() {
   return (
-   
-   <div>
-     hello
-   </div>
+      <BrowserRouter>
+      <Navbar/>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/signup">
+        <SignUp/>
+      </Route>
+      <Route path="/profile">
+        <Profile/>
+      </Route>
+
+      </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+import {Link} from 'react-router-dom'
+export default function Navbar() {
+    return (
+        <nav>
+        <div className="nav-wrapper white">
+            <Link to="/" className="brand-logo left">Instagram</Link>
+            <ul id="nav-mobile" className="right">
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="profile">Profile</Link></li>
+            </ul>
+        </div>
+        </nav>
+    )
+}
