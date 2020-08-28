fa0cd499371e652827f676697940b88d98ac0730
Amit Raj <49913746+amit1884@users.noreply.github.com>
2020-08-28 12:44:36 +0530

    logout implemented

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,29 @@
 import React ,{useContext}from 'react'
 import '../App.css'
-import {Link} from 'react-router-dom'
+import {Link,useHistory} from 'react-router-dom'
 import {UserContext} from '../App'
 export default function Navbar() {
-
+    const history=useHistory()
     const {state,dispatch}=useContext(UserContext)
     const renderList=()=>{
         if(state)
         {
             return [
                 <li><Link to="/profile">Profile</Link></li>,
-                <li><Link to ="/create">Create</Link></li>
+                <li><Link to ="/create">Create</Link></li>,
+                <li>
+                    <button 
+                    style={{marginRight:"20px"}}
+                        onClick={()=>{
+                            localStorage.clear()
+                            dispatch({type:"CLEAR"})
+                            history.push("/login")
+                        }}
+                    className="btn #c62828 red darken-3">
+                        {/* <i className="material-icons">power_settings_new</i> */}
+                        Logout
+                    </button>
+                </li>,
             ]
         }
         else{
